8499ff78c2ca551915ffc94ef1f18c22b8b5adb9
marcinjak9 <marcinjak9@gmail.com>
2017-08-22 18:58:55 +0200

    initial commit

---
+++ imports/ui/AddLink.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import Modal from 'react-modal';
+
+export default class AddLink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: '',
+      isOpen: false,
+    };
+  }
+  onSubmit(e) {
+    const { url } = this.state;
+
+    e.preventDefault();
+
+    Meteor.call('links.insert', url, (err, res) => {
+      if (!err) {
+        this.handleModalClose();
+      } else {
+        this.setState({ error: err.reason });
+      }
+    });
+  }
+  onChange(e) {
+    this.setState({
+      url: e.target.value
+    });
+  }
+
+  handleModalClose = () => {
+    this.setState({ isOpen: false, url: '', error: '' })
+  }
+
+  render() {
+    return (
+      <div>
+      <button onClick={() => this.setState({ isOpen: true })} className="button"> + Add Link</button>
+        <Modal 
+          isOpen={this.state.isOpen} 
+          contentLabel="Add Link"
+          onAfterOpen={() => this.refs.url.focus()}
+          onRequestClose={this.handleModalClose}
+          className="boxed-view__box"
+          overlayClassName="boxed-view boxed-view--modal"
+        >
+          <h1>Add Link</h1>
+          <p>{this.state.error ? this.state.error : ''}</p>
+          <form onSubmit={this.onSubmit.bind(this)} className="boxed-view__form">
+              <input
+                type="text"
+                placeholder="URL"
+                ref="url"
+                value={this.state.url}
+                onChange={this.onChange.bind(this)}/>
+              <button className="button">Add Link</button>
+              <button type="button" className="button button--secondary" onClick={this.handleModalClose}>Cancel</button>
+          </form>
+        </Modal>
+      </div>
+    );
+  }
+}
